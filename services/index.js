cc99ec2984eae0da780baf49fdd7af3ec642c00d
RodelinDipre <rdbivieca@gmail.com>
2020-07-15 23:02:40 -0400

    Servicios para las rutas

---
+++ services/index.js
@@ -0,0 +1,9 @@
+const OrderService = require('./OrderService');
+const ProductService = require('./ProductService');
+const ClientService = require('./ClientService');
+
+module.exports = {
+  OrderService,
+  ProductService,
+  ClientService,
+};
